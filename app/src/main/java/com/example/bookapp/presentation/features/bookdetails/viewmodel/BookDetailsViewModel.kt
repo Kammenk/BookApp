e495e2adc4ceb3f676a355c3cc442929e7fa10c5
kammmenk <kamen@lionheartbulgaria.io>
2023-02-15 21:19:06 +0200

    added a new viewmodel

---
+++ app/src/main/java/com/example/bookapp/presentation/features/bookdetails/viewmodel/BookDetailsViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.bookapp.presentation.features.bookdetails.viewmodel
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class BookDetailsViewModel: ViewModel() {
+}
